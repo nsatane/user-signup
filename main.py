21d4bd2e43e359748e750e23e5373351a90595f4
Neha Bhalke <nsatane@gmail.com>
2018-10-01 20:21:38 -0500

    Final changes made. Programm working now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect
 import cgi
 import os
+
 import re
 
 app = Flask(__name__)
@@ -17,8 +18,8 @@ def index():
 
 @app.route("/signup", methods=['GET','POST'])
 def signup():
-    Username= request.form['User_name']
-    password = request.form['password']
+    Username= request.form['Username']
+    password = request.form['Password']
     v_pass = request.form['Verify']
     email = request.form['email']
 
@@ -34,7 +35,7 @@ def signup():
     elif  len(Username) <3 and len(Username)>20:
         user_error = "The username must contain 3 to 20 character"
         Username = ''
-    elif '' in Username :
+    elif ' ' in Username :
         user_error = 'Username cannot contain any spaces'
         Username = ''
 
@@ -44,7 +45,7 @@ def signup():
     elif  len(password) <3 and len(password)>20:
         pass_error = "The password must contain 3 to 20 character"
         password = ''
-    elif '' in password :
+    elif ' ' in password :
         pass_error = 'Password cannot contain any spaces'
         password = ''
 
@@ -57,17 +58,18 @@ def signup():
     if email != "": # Validate Email
         # Used regex for complete email validation.
         if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-                email_error = "Not a valid email address."
+            email_error = "Not a valid email address."
 
 
-    if not username_error and not pass_error and not verify_error and not email_error:
-        return render_template('welcome.html', username = Username)
+    if not user_error and not pass_error and not verify_error and not email_error:
+        return render_template('welcome.html', Username = Username)
     else:
         return render_template(
             'index.html',
-            username = username,
-            username_error = username_error,
-            pass_error = pass_error,
+            username = Username,
+            username_error = user_error,
+            password = password,
+            pass_error = pass_error, v_pass=v_pass,
             verify_error = verify_error,
             email = email,
             email_error = email_error
